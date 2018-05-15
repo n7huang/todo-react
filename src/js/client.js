fe1633a56c6ed0426fcfa8d43b2ff16f533f826d
Nicholas Huang <nicholas.huang@chefhero.com>
2018-05-15 14:21:54 -0400

    move todo item to its own component

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import ToDoContainer from "./components/ToDoContainer";
+import ToDoContainer from "./components/ToDoContainer.jsx";
 
 const app = document.getElementById('app');
 ReactDOM.render(<ToDoContainer/>, app);
--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -15,58 +15,55 @@ export default class ToDoList extends React.Component {
 		editing: null
 	}
 
-	handleRemove(todoId) {
+	handleRemove = (todoId) => {
 		this.props.removeToDo(todoId);
 	}
 
-	handleEdit(e, todoId) {
-		if ( e.keyCode === 13 ) {	// on enter key press
-			this.props.editToDo(e.target.value, todoId);
-			this.setState({editing: null});
-		}
+	handleEdit = (e, todoId) => {
+		this.props.editToDo(e.target.value, todoId);
 	}
 
 	handleAdd = (e) => {
     this.props.addToDo("<Click To Edit>");
   }
 
-  toggleEdit(todoId) {
-  	this.setState({editing: todoId});
-  }
-
   // changed to edit based on index instead
-  renderItemOrEdit(todo) {
-  	if (this.state.editing === todo.id) {
-  		return (
-  			<li key={ `editing-${todo.id}` } className="todo-list-item">
-  				<button onClick={() => this.handleRemove(todo.id)} >Remove</button>
-  				<input autoFocus
-  					onKeyDown={e => this.handleEdit(e, todo.id)}
-  					type="text"
-  					className="ctrl"
-  				/>
-  			</li>
-  		)
-  	} else {
-  		return (
-  			<li key={todo.id}
-  					className="todo-list-item">
-  				<button onClick={() => this.handleRemove(todo.id)} >Remove</button>
-  				<span onClick={() => this.toggleEdit(todo.id)}>{todo.text}</span>
-  			</li>
-  		)
-  	}
-  }
+  // renderItemOrEdit(todo) {
+  // 	if (this.state.editing === todo.id) {
+  // 		return (
+  // 			<li key={ `editing-${todo.id}` } className="todo-list-item">
+  // 				<button onClick={() => this.handleRemove(todo.id)} >Remove</button>
+  // 				<input autoFocus
+  // 					onKeyDown={e => this.handleEdit(e, todo.id)}
+  // 					type="text"
+  // 					className="ctrl"
+  // 				/>
+  // 			</li>
+  // 		)
+  // 	} else {
+  // 		return (
+  // 			<li key={todo.id}
+  // 					className="todo-list-item">
+  // 				<button onClick={() => this.handleRemove(todo.id)} >Remove</button>
+  // 				<span onClick={() => this.toggleEdit(todo.id)}>{todo.text}</span>
+  // 			</li>
+  // 		)
+  // 	}
+  // }
 
+  /*this.renderItemOrEdit(todo)*/ // was in map
   render() {
   	return (
   		<div>
+        <button onClick={this.handleAdd}>Add New Item</button>
 	  		<ul className="todo-list">
 		  		{ this.props.todos.map( todo => 
-		  			this.renderItemOrEdit(todo)
+            <ToDoItem key={todo.id} 
+              todo={todo}
+              handleRemove={this.handleRemove}
+              handleEdit={this.handleEdit} />
 		  		)}
 	  		</ul>
-	  		<button onClick={this.handleAdd}>Add New Item</button>
   		</div>
   	);
   }
